fafd9366b812c84e1b211a9562bc36e3e0d421f5
adityamusthofa67 <adityamusthofa67@gmail.com>
2019-12-01 23:52:34 +0700

    animation 2D, bug in triangle vertical

--- main.cpp
+++ main.cpp
@@ -1,138 +1,234 @@
 /*
- * GLUT Shapes Demo
+ * GLUT HomeWork Animation 2D
  *
- * Written by Nigel Stewart November 2003
+ * Written by Adhitya Musthofa December 2019
  *
- * This program is test harness for the sphere, cone
- * and torus shapes in GLUT.
+ * This program is test animation 2D in GLUT.
  *
- * Spinning wireframe and smooth shaded shapes are
- * displayed until the ESC or q key is pressed.  The
- * number of geometry stacks and slices can be adjusted
- * using the + and - keys.
  */
 
 #include <windows.h>
 #ifdef __APPLE__
-#include <GLUT/glut.h>
 #else
 #include <GL/glut.h>
 #endif
 
 #include <stdlib.h>
+#include <math.h>
 
-static int slices = 16;
-static int stacks = 16;
+/* struck declare */
 
-/* GLUT callback Handlers */
+typedef struct {
+    float x, y;
+} point2D_t;
 
-static void resize(int width, int height)
-{
-    const float ar = (float) width / (float) height;
+typedef struct {
+    float r, g, b;
+} color_t;
 
-    glViewport(0, 0, width, height);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glFrustum(-ar, ar, -1.0, 1.0, 2.0, 100.0);
+/* triangle declare */
 
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity() ;
+point2D_t triangleHorizontal[3] = {{-310, 200}, {-310, 160}, {-260, 180}};
+point2D_t triangleVertical[3] = {{-310, -200}, {-270, -200}, {-290, -170}};
+
+/* circle declare */
+
+point2D_t circleHorizontal[1] = {{-300, 100}};
+point2D_t circleVertical[1] = {{-250, -200}};
+
+/* star declare */
+
+point2D_t starHorizontal[10] = {{-280, 80}, {-275, 70}, {-256, 70},
+{-275, 65}, {-270, 55}, {-280, 60}, {-290, 55}, {-285, 65}, {-295, 70}, {-285, 70}};
+point2D_t starVertical[10] = {{-180, -230}, {-175, -220}, {-165, -220},
+{-175, -215}, {-170, -205}, {-180, -210}, {-190, -205}, {-185, -215}, {-195, -220}, {-185, -220}};
+
+void setColor(color_t col) {
+    glColor3f(col.r, col.g, col.b);
 }
 
-static void display(void)
-{
-    const double t = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
-    const double a = t*90.0;
+void drawPolyline(point2D_t pnt[], int n, color_t color) {
+    int i;
+    setColor(color);
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor3d(1,0,0);
-
-    glPushMatrix();
-        glTranslated(-2.4,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidSphere(1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(0,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidCone(1,1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(2.4,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidTorus(0.2,0.8,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(-2.4,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireSphere(1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(0,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireCone(1,1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(2.4,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireTorus(0.2,0.8,slices,stacks);
-    glPopMatrix();
-
-    glutSwapBuffers();
+    glBegin(GL_LINE_LOOP);
+    for(i=0; i<n; i++) {
+        glVertex2f(pnt[i].x, pnt[i].y);
+    }
+    glEnd();
 }
 
+void drawCircle(point2D_t pnt[], int radio, color_t color) {
+    int i;
+    float theta;
+    setColor(color);
 
-static void key(unsigned char key, int x, int y)
-{
-    switch (key)
-    {
-        case 27 :
-        case 'q':
-            exit(0);
-            break;
+    glBegin(GL_POLYGON);
+    for(i=0; i<360; i++) {
+        theta = i*3.1415/180;
+        glVertex2f(pnt[0].x + radio*cos(theta), pnt[0].y + radio*sin(theta));
+    }
+    glEnd();
+}
 
-        case '+':
-            slices++;
-            stacks++;
-            break;
+void triangle_hori() {
+    int i;
+    color_t col = {1.0, 0.0, 0.0};
+    drawPolyline(triangleHorizontal, 3, col);
 
-        case '-':
-            if (slices>3 && stacks>3)
-            {
-                slices--;
-                stacks--;
-            }
-            break;
+    for(i=0; i<3; i++) {
+        triangleHorizontal[i].x += 5;
     }
 
-    glutPostRedisplay();
+    if(triangleHorizontal[0].x > 320) {
+
+        triangleHorizontal[0].x = -310, 0;
+        triangleHorizontal[1].x = -310, 0;
+        triangleHorizontal[2].x = -260, 0;
+
+        triangleHorizontal[0].y = 200, 0;
+        triangleHorizontal[1].y = 160, 0;
+        triangleHorizontal[2].y = 180, 0;
+    }
 }
 
-static void idle(void)
-{
-    glutPostRedisplay();
+void drawCircleHori() {
+    color_t col = {0.0, 1.0, 0.0};
+    drawCircle(circleHorizontal, 10, col);
+    circleHorizontal[0].x += 4;
+
+    if(circleHorizontal[0].x > 300) {
+        circleHorizontal[0].x = -300;
+        circleHorizontal[0].y = 100;
+    }
 }
 
-const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
-const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f };
+void star_hori() {
+    int i;
+    color_t col = {0.0, 0.0, 1.0};
+    drawPolyline(starHorizontal, 10, col);
 
-const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
-const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
-const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat high_shininess[] = { 100.0f };
+    for(i=0; i<10; i++) {
+        starHorizontal[i].x += 3;
+    }
+
+    if(starHorizontal[3].x > 320) {
+
+        starHorizontal[0].x = -280, 0;
+        starHorizontal[1].x = -275, 0;
+        starHorizontal[2].x = -265, 0;
+        starHorizontal[3].x = -275, 0;
+        starHorizontal[4].x = -270, 0;
+        starHorizontal[5].x = -280, 0;
+        starHorizontal[6].x = -290, 0;
+        starHorizontal[7].x = -285, 0;
+        starHorizontal[8].x = -295, 0;
+        starHorizontal[9].x = -285, 0;
+
+        starHorizontal[0].y = 80, 0;
+        starHorizontal[1].y = 70, 0;
+        starHorizontal[2].y = 70, 0;
+        starHorizontal[3].y = 65, 0;
+        starHorizontal[4].y = 55, 0;
+        starHorizontal[5].y = 60, 0;
+        starHorizontal[6].y = 55, 0;
+        starHorizontal[7].y = 65, 0;
+        starHorizontal[8].y = 70, 0;
+        starHorizontal[9].y = 70, 0;
+    }
+}
+
+void triangle_verti() {
+    int i;
+    color_t col = {1.0, 0.0, 0.0};
+    drawPolyline(triangleVertical, 3, col);
+
+    for(i=0; i<3; i++) {
+        triangleVertical[i].y += 5;
+    }
+
+    if(triangleVertical[2].y > 240) {
+
+        triangleVertical[0].x = -310, 0;
+        triangleVertical[1].x = -270, 0;
+        triangleVertical[2].x = -290, 0;
+
+        triangleVertical[0].y = -200, 0;
+        triangleVertical[0].y = -200, 0;
+        triangleVertical[0].y = -170, 0;
+    }
+}
+
+void drawCircleVerti() {
+    color_t col = {0.0, 1.0, 0.0};
+    drawCircle(circleVertical, 20, col);
+    circleVertical[0].y += 4;
+
+    if(circleVertical[0].y > 280) {
+
+        circleVertical[0].x = -250;
+        circleVertical[0].y = -200;
+    }
+}
+
+void star_verti() {
+    int i;
+    color_t col = {0.0, 0.0, 1.0};
+    drawPolyline(starVertical, 10, col);
+
+    for(i=0; i<10; i++) {
+        starVertical[i].y += 3;
+    }
+
+    if(starVertical[3].y > 240) {
+
+        starVertical[0].x = -180.0;
+        starVertical[1].x = -175.0;
+        starVertical[2].x = -165.0;
+        starVertical[3].x = -175.0;
+        starVertical[4].x = -170.0;
+        starVertical[5].x = -180.0;
+        starVertical[6].x = -190.0;
+        starVertical[7].x = -185.0;
+        starVertical[8].x = -195.0;
+        starVertical[9].x = -185.0;
+
+        starVertical[0].y = -230.0;
+        starVertical[1].y = -220.0;
+        starVertical[2].y = -220.0;
+        starVertical[3].y = -210.0;
+        starVertical[4].y = -205.0;
+        starVertical[5].y = -210.0;
+        starVertical[6].y = -205.0;
+        starVertical[7].y = -215.0;
+        starVertical[8].y = -220.0;
+        starVertical[9].y = -220.0;
+    }
+}
+
+void timer(int value) {
+    glutPostRedisplay();
+    glutTimerFunc(10, timer, 0);
+}
+
+void userDraw() {
+    glClearColor(0., 0., 0., 0.);
+
+    triangle_hori();
+    triangle_verti();
+
+    star_hori();
+    star_verti();
+
+    drawCircleHori();
+    drawCircleVerti();
+}
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    userDraw();
+    glutSwapBuffers();
+}
 
 /* Program entry point */
 
@@ -141,36 +237,16 @@ int main(int argc, char *argv[])
     glutInit(&argc, argv);
     glutInitWindowSize(640,480);
     glutInitWindowPosition(10,10);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
 
-    glutCreateWindow("GLUT Shapes");
+    glutCreateWindow("Created by Adhitya Musthofa 2103187091");
 
-    glutReshapeFunc(resize);
+    glClearColor(0,0,0,0);
+    gluOrtho2D(-320.0, 320.0, -240.0, 240.0);
+
     glutDisplayFunc(display);
-    glutKeyboardFunc(key);
-    glutIdleFunc(idle);
 
-    glClearColor(1,1,1,1);
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
-
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LESS);
-
-    glEnable(GL_LIGHT0);
-    glEnable(GL_NORMALIZE);
-    glEnable(GL_COLOR_MATERIAL);
-    glEnable(GL_LIGHTING);
-
-    glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-
-    glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
-    glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
-    glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
+    glutTimerFunc(10, timer, 0);
 
     glutMainLoop();
 
